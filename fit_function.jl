ab0946c9c074a7c78a8c58826924586756d39057
framere <francesco.mereto@rwth-aachen.de>
2025-08-25 10:56:05 +0200

    Plots

--- fit_function.jl
+++ fit_function.jl
@@ -4,14 +4,14 @@ using LsqFit
 using Plots
 
 # read the data from potential_data
-data = read("potential_data", skipheader=1)
+data = readdlm("potential_data", skipstart=1)
 
 x = data[:, 1]
 E = data[:, 2]
 
-# define the model
-model(x, p) = p[1] .+ p[2] * x.^2 .+ p[3] * x.^3
-initial_params = [0.0, 1.0, 0.0]
+# define the model function and fit
+model(x, p) = p[1] .+ p[2] * x .+ p[3] * x.^2 .+ p[4] * x.^3
+initial_params = [1000.0, - 1.0, 0.0, 0.0]
 fit_result = curve_fit(model, x, E, initial_params)
 
 best_fit_params = fit_result.param
@@ -19,5 +19,6 @@ best_fit_params = fit_result.param
 println("Best fit parameters: ", best_fit_params)
 
 # plot the data and the fit
-scatter(x, E, label="Data", xlabel="x", ylabel="E", title="Data and Fit")
-plot!(x, model(x, best_fit_params), label="Fit", linewidth=2)
+x_plot = range(minimum(x), stop=maximum(x), length=100)
+scatter(x, E, label="Data", xlabel="x", ylabel="E [eV]", title="CO Vibrational Potential on Pt(111) Surface")
+plot!(x_plot, model(x_plot, best_fit_params), label="Fit", linewidth=2)
