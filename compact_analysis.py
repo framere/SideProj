05d5bae72298a0a604ffe2df60c13e7773f52f94
framere <francesco.mereto@rwth-aachen.de>
2025-10-28 14:48:16 +0100

    debug

--- compact_analysis.py
+++ compact_analysis.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.constants import e, hbar, angstrom, physical_constants
 import scipy.constants as const
 import help_functions as hf
 
@@ -9,69 +10,40 @@ E_data = data[:, 1]  # Energy values in eV
 E_shifted = E_data - E_data.min()  # Shift energies so that minimum is at 0 eV
 
 # ============================= Define constants and parameters =============================
-e = const.elementary_charge  # Elementary charge in Coulombs
-mass_u = const.physical_constants['atomic mass constant'][0]  # Atomic mass unit in kg
-hbar = const.hbar  # Reduced Planck's constant in J·s
-angstrom = 1e-10  # 1 Angstrom in meters
+mass_u = physical_constants['atomic mass constant'][0]  # Atomic mass unit in kg
 omega = 265.495409 *1e-3 * e / hbar  # Convert meV to J and then to angular frequency
 m_eff = 13.585135873543415 * mass_u  # Effective mass in kg
 
+# ============================= Select model to fit =============================
+MODEL = 'Morse'  # choose between 'quadratic', 'polynomial', and 'morse'
 
-# ============================= Fit models to data =============================
-guess_quadratic = [0.0, 100.0, 0.0]  # [offset, curvature, center]
-params_quadratic, _ = hf.fit_model(x_data, hf.quadratic_model, guess_quadratic, E_shifted)
+if MODEL == 'Quadratic':
+    potential_func = hf.quadratic_model
+    guess_quadratic = [0.0, 100.0, 0.0]  # [offset, curvature, center]
+    params_quadratic, _ = hf.fit_model(x_data, hf.quadratic_model, guess_quadratic, E_shifted)
+    potential_params = params_quadratic
+elif MODEL == 'Polynomial':
+    potential_func = hf.polynomial_model
+    guess_polynomial = [0.0, 0.0, -1.0, 1000.0]  # [a3, a2, a1, a0]
+    params_polynomial, _ = hf.fit_model(x_data, hf.polynomial_model, guess_polynomial, E_shifted)
+    potential_params = params_polynomial
+elif MODEL == 'Morse':
+    potential_func = hf.morse_model
+    guess_morse = [E_shifted.max() - E_shifted.min(), 1.0, x_data.mean(), E_shifted.min()]  # Initial guess for [D_e, a, r_e, offset]
+    params_morse, _ = hf.fit_model(x_data, hf.morse_model, guess_morse, E_shifted)
+    potential_params = params_morse
 
-guess_polynomial = [0.0, 0.0, -1.0, 1000.0]  # [a3, a2, a1, a0]
-params_polynomial, _ = hf.fit_model(x_data, hf.polynomial_model, guess_polynomial, E_shifted)
+num_levels = 2  # number of energy levels to compute    
 
-guess_morse = [E_shifted.max() - E_shifted.min(), 1.0, x_data.mean(), E_shifted.min()]  # Initial guess for [D_e, a, r_e, offset]
-params_morse, _ = hf.fit_model(x_data, hf.morse_model, guess_morse, E_shifted)
 
 # ============================= Plot results =============================
-hf.plot_fits(x_data, E_shifted, params_quadratic, params_polynomial, params_morse)
-
-
-# ============================= Analytical solution Morse =============================
-# convert in SI units for further calculations
-D_e = params_morse[0] * e  # in J
-a = params_morse[1] / angstrom  # in 1/m
-r_e = params_morse[2] * angstrom  # in m
-offset = params_morse[3] * e  # in J
-
-# Solving the Schrödinger equation
-lambda_param = np.sqrt(2 * m_eff * D_e) / (a * hbar) # see parametrization wikipedia
-n_max = int(2 * lambda_param - 0.5)
-energy_levels = []
-N_energy_levels = 2
-for n in range(N_energy_levels):
-    epsilon_n = (2 * lambda_param - n - 0.5) * (n + 0.5) * (hbar * a)**2 / (2 * m_eff)
-    energy_levels.append(epsilon_n + offset)
-
-print("Analytical energy levels (in eV):")
-for i, energy in enumerate(energy_levels):
-    print(f"n={i}: {energy / e:.4f} eV")
-
-x_vals = np.linspace(-0.14, 0.23, 1000)
-hf.plot_energy_levels(params_morse, energy_levels, x_vals, e)
+hf.plot_fit(x_data, E_shifted, potential_func, potential_params, MODEL)
 
 # ============================ Numerical calculations =============================
 # --- build grid ----
 x_min = -0.1
 x_max = 0.15
 N = 2000     
-num_levels = 2  # number of energy levels to compute
-
-MODEL = 'quadratic'  # choose between 'quadratic', 'polynomial', and 'morse'
-if MODEL == 'quadratic':
-    potential_func = hf.quadratic_model
-    potential_params = params_quadratic
-elif MODEL == 'polynomial':
-    potential_func = hf.polynomial_model
-    potential_params = params_polynomial
-elif MODEL == 'morse':
-    potential_func = hf.morse_model
-    potential_params = params_morse
-
 # --- solve schrodinger ---
 x_A, E_numerical, PSI_numerical = hf.solve_schrodinger(m_eff, x_min, x_max, N, potential_func, potential_params, num_levels, e, hbar)
 
@@ -79,4 +51,19 @@ print("Numerical energy levels (in eV):")
 for n, energy in enumerate(E_numerical):
     print(f"n={n}: {energy:.4f} eV")
 # --- plot numerical energy levels ---
-hf.plot_numerical_energy_levels(x_A, potential_func, potential_params, E_numerical, PSI_numerical)
+hf.plot_numerical_energy_levels(x_A, potential_func, potential_params, E_numerical, PSI_numerical, MODEL)
+
+
+# ============================= Analytical solution Morse =============================
+if MODEL != 'Morse':
+    raise ValueError("Analytical Morse solution can only be computed if the Morse model is selected.")
+
+# --- compute analytical energy levels ---
+energy_levels = hf.analytical_morse_energy_levels(params_morse, m_eff, hbar, e, num_levels)
+
+print("Analytical energy levels (in eV):")
+for i, energy in enumerate(energy_levels):
+    print(f"n={i}: {energy / e:.4f} eV")
+
+x_vals = np.linspace(x_min, x_max, 1000)
+hf.plot_energy_levels(params_morse, energy_levels, x_vals, e)
--- help_functions.py
+++ help_functions.py
@@ -28,47 +28,19 @@ def fit_model(x, model, guess, E_shifted):
 
 
 # plot results function
-def plot_fits(x_data, E_shifted, params_quadratic, params_polynomial, params_morse):
+def plot_fit(x_data, E_shifted, model, params, name):
     X_axis = np.linspace(x_data.min()*1.1, x_data.max()*1.1, 200)
     plt.figure(figsize=(6, 4))
     plt.scatter(x_data, E_shifted, label='Data', marker='^', color=colors[3], s=40, zorder=5)
-    plt.plot(X_axis, quadratic_model(X_axis, *params_quadratic), label='Quadratic Fit', color=colors[0])
-    plt.plot(X_axis, polynomial_model(X_axis, *params_polynomial), label='Polynomial Fit', color=colors[1])
-    plt.plot(X_axis, morse_model(X_axis, *params_morse), label='Morse Fit', color=colors[2])
+    plt.plot(X_axis, model(X_axis, *params), label=f'{name} Fit', color=colors[0])
     plt.xlabel('Position (Angstrom)')
     plt.ylabel('Energy (eV)')
     plt.title('Potential Energy Surface Fitting')
     plt.legend()
     plt.grid()
     sns.despine()
-    plt.savefig(f'{folder}potential_fits.pdf', bbox_inches='tight')
+    plt.savefig(f'{folder}potential_fit_{name}.pdf', bbox_inches='tight')
     plt.close()
-
-# ============================= Analytical solution Morse =============================
-# plot energy levels function
-def plot_energy_levels(params_morse, energy_levels, x_vals, e):
-    plt.figure(figsize=(6, 4))
-    plt.plot(x_vals, morse_model(x_vals, *params_morse), label='Morse Fit', color=colors[2])
-    for n, energy in enumerate(energy_levels):
-        min_index = np.where(morse_model(x_vals, *params_morse) <= energy / e)[0][0]
-        max_index = np.where(morse_model(x_vals, *params_morse) <= energy / e)[0][-1]
-        xi, xj = x_vals[min_index], x_vals[max_index]
-        plt.hlines(energy / e, xi, xj, colors='r', label='Energy levels' if n == 0 else "")
-        # place level label centered above the line
-        xmid = 0.5 * (xi + xj)
-        vp = morse_model(x_vals, *params_morse)
-        y_offset = 0.01 * (vp.max() - vp.min())
-        plt.text(xmid, energy / e + y_offset, f"n={n}", ha='center', va='bottom', color='r', fontsize=8)
-        
-    plt.xlabel('Position (Angstrom)')
-    plt.ylabel('Energy (eV)')
-    plt.title('Morse Potential with Energy Levels')
-    plt.legend()
-    sns.despine()
-    plt.grid()
-    plt.savefig(f'{folder}morse_energy_levels.pdf', bbox_inches='tight')
-    plt.close()
-
 
 # ============================ Numerical solution ============================
 def solve_schrodinger(m_eff, x_min, x_max, N, potential_func, potential_params, num_levels, e, hbar):
@@ -106,7 +78,7 @@ def solve_schrodinger(m_eff, x_min, x_max, N, potential_func, potential_params, 
     return x_A, E_eV, PSI
 
 # plot numerical energy levels function
-def plot_numerical_energy_levels(x_A, potential_func, potential_params, E_eV, PSI):
+def plot_numerical_energy_levels(x_A, potential_func, potential_params, E_eV, PSI, model):
     plt.figure(figsize=(6,4))
     V_eV = potential_func(x_A, *potential_params)
     plt.plot(x_A, V_eV, 'k', lw=1.5, label='V(x)')
@@ -130,5 +102,48 @@ def plot_numerical_energy_levels(x_A, potential_func, potential_params, E_eV, PS
     plt.title('Bound states from finite difference')
     plt.grid(True)
     sns.despine()
-    plt.savefig(f'{folder}numerical_energy_levels.pdf', bbox_inches='tight')
+    plt.savefig(f'{folder}numerical_energy_levels_{model}.pdf', bbox_inches='tight')
+    plt.close()
+
+
+# ============================= Analytical solution Morse =============================
+def analytical_morse_energy_levels(params_morse, m_eff, hbar, e, N_energy_levels):
+    # convert in SI units for further calculations
+    D_e = params_morse[0] * e  # in J
+    a = params_morse[1] / 1e-10  # in 1/m
+    r_e = params_morse[2] * 1e-10  # in m
+    offset = params_morse[3] * e  # in J
+
+    # Solving the Schrödinger equation
+    lambda_param = np.sqrt(2 * m_eff * D_e) / (a * hbar) # see parametrization wikipedia
+    n_max = int(2 * lambda_param - 0.5)
+    energy_levels = []
+    for n in range(N_energy_levels):
+        epsilon_n = (2 * lambda_param - n - 0.5) * (n + 0.5) * (hbar * a)**2 / (2 * m_eff)
+        energy_levels.append(epsilon_n + offset)
+
+    return energy_levels
+
+# plot energy levels function
+def plot_energy_levels(params_morse, energy_levels, x_vals, e):
+    plt.figure(figsize=(6, 4))
+    plt.plot(x_vals, morse_model(x_vals, *params_morse), label='Morse Fit', color=colors[2])
+    for n, energy in enumerate(energy_levels):
+        min_index = np.where(morse_model(x_vals, *params_morse) <= energy / e)[0][0]
+        max_index = np.where(morse_model(x_vals, *params_morse) <= energy / e)[0][-1]
+        xi, xj = x_vals[min_index], x_vals[max_index]
+        plt.hlines(energy / e, xi, xj, colors='r', label='Energy levels' if n == 0 else "")
+        # place level label centered above the line
+        xmid = 0.5 * (xi + xj)
+        vp = morse_model(x_vals, *params_morse)
+        y_offset = 0.01 * (vp.max() - vp.min())
+        plt.text(xmid, energy / e + y_offset, f"n={n}", ha='center', va='bottom', color='r', fontsize=8)
+        
+    plt.xlabel('Position (Angstrom)')
+    plt.ylabel('Energy (eV)')
+    plt.title('Bound states from analytical Morse solution')
+    plt.legend()
+    sns.despine()
+    plt.grid()
+    plt.savefig(f'{folder}morse_energy_levels.pdf', bbox_inches='tight')
     plt.close()
