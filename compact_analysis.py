cd839839400c5c022ba7c2164b046da20c24bc5e
framere <francesco.mereto@rwth-aachen.de>
2025-10-28 14:53:57 +0100

    change name

--- compact_analysis.py
+++ compact_analysis.py
@@ -1,7 +1,7 @@
 import numpy as np
-from scipy.constants import e, hbar, angstrom, physical_constants
+from scipy.constants import e, hbar, physical_constants
 import scipy.constants as const
-import help_functions as hf
+import helpers as hp
 
 # ============================ Load data =============================
 data = np.loadtxt('potential_data', skiprows=1)
@@ -18,26 +18,25 @@ m_eff = 13.585135873543415 * mass_u  # Effective mass in kg
 MODEL = 'Morse'  # choose between 'quadratic', 'polynomial', and 'morse'
 
 if MODEL == 'Quadratic':
-    potential_func = hf.quadratic_model
+    potential_func = hp.quadratic_model
     guess_quadratic = [0.0, 100.0, 0.0]  # [offset, curvature, center]
-    params_quadratic, _ = hf.fit_model(x_data, hf.quadratic_model, guess_quadratic, E_shifted)
+    params_quadratic, _ = hp.fit_model(x_data, hp.quadratic_model, guess_quadratic, E_shifted)
     potential_params = params_quadratic
 elif MODEL == 'Polynomial':
-    potential_func = hf.polynomial_model
+    potential_func = hp.polynomial_model
     guess_polynomial = [0.0, 0.0, -1.0, 1000.0]  # [a3, a2, a1, a0]
-    params_polynomial, _ = hf.fit_model(x_data, hf.polynomial_model, guess_polynomial, E_shifted)
+    params_polynomial, _ = hp.fit_model(x_data, hp.polynomial_model, guess_polynomial, E_shifted)
     potential_params = params_polynomial
 elif MODEL == 'Morse':
-    potential_func = hf.morse_model
+    potential_func = hp.morse_model
     guess_morse = [E_shifted.max() - E_shifted.min(), 1.0, x_data.mean(), E_shifted.min()]  # Initial guess for [D_e, a, r_e, offset]
-    params_morse, _ = hf.fit_model(x_data, hf.morse_model, guess_morse, E_shifted)
+    params_morse, _ = hp.fit_model(x_data, hp.morse_model, guess_morse, E_shifted)
     potential_params = params_morse
 
 num_levels = 2  # number of energy levels to compute    
 
-
-# ============================= Plot results =============================
-hf.plot_fit(x_data, E_shifted, potential_func, potential_params, MODEL)
+# ============================= Plot results fit =============================
+hp.plot_fit(x_data, E_shifted, potential_func, potential_params, MODEL)
 
 # ============================ Numerical calculations =============================
 # --- build grid ----
@@ -45,13 +44,13 @@ x_min = -0.1
 x_max = 0.15
 N = 2000     
 # --- solve schrodinger ---
-x_A, E_numerical, PSI_numerical = hf.solve_schrodinger(m_eff, x_min, x_max, N, potential_func, potential_params, num_levels, e, hbar)
+x_A, E_numerical, PSI_numerical = hp.solve_schrodinger(m_eff, x_min, x_max, N, potential_func, potential_params, num_levels, e, hbar)
 
 print("Numerical energy levels (in eV):")
 for n, energy in enumerate(E_numerical):
     print(f"n={n}: {energy:.4f} eV")
 # --- plot numerical energy levels ---
-hf.plot_numerical_energy_levels(x_A, potential_func, potential_params, E_numerical, PSI_numerical, MODEL)
+hp.plot_numerical_energy_levels(x_A, potential_func, potential_params, E_numerical, PSI_numerical, MODEL)
 
 
 # ============================= Analytical solution Morse =============================
@@ -59,11 +58,11 @@ if MODEL != 'Morse':
     raise ValueError("Analytical Morse solution can only be computed if the Morse model is selected.")
 
 # --- compute analytical energy levels ---
-energy_levels = hf.analytical_morse_energy_levels(params_morse, m_eff, hbar, e, num_levels)
+energy_levels = hp.analytical_morse_energy_levels(params_morse, m_eff, hbar, e, num_levels)
 
 print("Analytical energy levels (in eV):")
 for i, energy in enumerate(energy_levels):
     print(f"n={i}: {energy / e:.4f} eV")
 
 x_vals = np.linspace(x_min, x_max, 1000)
-hf.plot_energy_levels(params_morse, energy_levels, x_vals, e)
+hp.plot_energy_levels(params_morse, energy_levels, x_vals, e)
